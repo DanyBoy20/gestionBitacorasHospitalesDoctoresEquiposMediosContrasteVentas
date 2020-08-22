e76fd00b2c56f1eab5f4322768d1a98a1a2120ee
Dany Hernandez <foxdan20@hotmail.com>
2020-08-22 14:18:48 -0500

    Arreglo de error sesion ya iniciada

--- controlador/ingreso.php
+++ controlador/ingreso.php
@@ -1,20 +1,21 @@
 <?php
-session_start();
 class Ingreso{
 	static public function ingresoController(){
 		if(isset($_POST["usuarioIngreso"])){
 			$datosController = array("usuario"=>$_POST["usuarioIngreso"], "password"=>$_POST["passwordIngreso"]);
 			$respuesta = IngresoModels::ingresoModel($datosController);
 			if($respuesta["emailpersonal"] == $_POST["usuarioIngreso"] && $respuesta["passwordpersonal"] == $_POST["passwordIngreso"]){
+				session_start();
 				$_SESSION["validar"] = true;
 				$_SESSION["usuario"] = $respuesta["idpersonal"];
 				$_SESSION["nombre"] = $respuesta["nombrepersonal"];
 				//echo '<script> alert("bienvenido");</script>';
-				# echo '<script> windows.location = "inicio" ;</script>';
+				// echo '<script> windows.location = "inicio" ;</script>';
 				header("location:inicio");
 			}else{
 				echo "<p id='popuperror'>DATOS INCORRECTOS</p>";
 			}
+
 		}	
 	}
 }
